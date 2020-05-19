b77d3c4e60d1149d65ac06c47a2549f658178d42
YukiLeafX <yukileafx@gmail.com>
2020-05-20 01:19:53 +0900

    スクリプト更新

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,48 +4,53 @@ set -ex
 
 scripts/init.sh
 
-mkdir -p work
-if [ ! -d work/submods ]; then
-  cp -rf modules work/submods
-fi
+top="$(pwd -P)"
 
 (
+  mkdir -p work
   cd work
 
+  if [ ! -d data ]; then
+    cp -r "$top"/modules/BuildData data
+  fi
+
   working="$(pwd -P)"
-  builddata="$working"/submods/BuildData
-  ver=$(jq -r '.minecraftVersion' "$builddata"/info.json)
+  ver=$(jq -r '.minecraftVersion' data/info.json)
 
   if [ ! -f "$ver".jar ]; then
-    curl -o "$ver".jar "$(jq -r '.serverUrl' "$builddata"/info.json)"
-    [ "$(md5sum "$ver".jar | cut -d ' ' -f 1)" = "$(jq -r '.minecraftHash' "$builddata"/info.json)" ] || return
+    curl -o "$ver".jar "$(jq -r '.serverUrl' data/info.json)"
+    [ "$(md5sum "$ver".jar | cut -d ' ' -f 1)" = "$(jq -r '.minecraftHash' data/info.json)" ] || return
   fi
 
   if [ ! -f "$ver"-class.jar ]; then
-    java -jar "$builddata"/bin/SpecialSource-2.jar map \
+    java -jar data/bin/SpecialSource-2.jar map \
       -i "$ver".jar \
-      -m "$builddata"/mappings/"$(jq -r '.classMappings' "$builddata"/info.json)" \
+      -m data/mappings/"$(jq -r '.classMappings' data/info.json)" \
       -o "$ver"-class.jar
   fi
 
   if [ ! -f "$ver"-member.jar ]; then
-    java -jar "$builddata"/bin/SpecialSource-2.jar map \
+    java -jar data/bin/SpecialSource-2.jar map \
       -i "$ver"-class.jar \
-      -m "$builddata"/mappings/"$(jq -r '.memberMappings' "$builddata"/info.json)" \
+      -m data/mappings/"$(jq -r '.memberMappings' data/info.json)" \
       -o "$ver"-member.jar
   fi
 
   if [ ! -f "$ver"-mapped.jar ]; then
-    java -jar "$builddata"/bin/SpecialSource.jar \
+    java -jar data/bin/SpecialSource.jar \
       --kill-lvt \
       -i "$ver"-member.jar \
-      --access-transformer "$builddata"/mappings/"$(jq -r '.accessTransforms' "$builddata"/info.json)" \
-      -m "$builddata"/mappings/"$(jq -r '.packageMappings' "$builddata"/info.json)" \
+      --access-transformer data/mappings/"$(jq -r '.accessTransforms' data/info.json)" \
+      -m data/mappings/"$(jq -r '.packageMappings' data/info.json)" \
       -o "$ver"-mapped.jar
   fi
 
+  if [ ! -d cb ]; then
+    cp -r "$top"/modules/CraftBukkit cb
+  fi
+
   (
-    cd submods/CraftBukkit
+    cd cb
     mvn install:install-file \
       -Dfile="$working"/"$ver"-mapped.jar \
       -Dpackaging=jar \
@@ -53,4 +58,14 @@ fi
       -DartifactId=minecraft-server \
       -Dversion="$ver"-SNAPSHOT
   )
+
+  if [ ! -d nms ]; then
+    mkdir -p nms
+    (
+      cd nms
+      jar -xf "$working"/"$ver"-mapped.jar net/minecraft/server
+      java -jar "$working"/data/bin/fernflower.jar -dgs=1 -hdc=0 -asc=1 -udv=0 . .
+      find . -type f -iname '*.class' -print0 | xargs -0 rm -f
+    )
+  fi
 )
