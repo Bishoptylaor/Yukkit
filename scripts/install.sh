75eb730693c5f45b900f0ef67a97e913965b8971
YukiLeafX <yukileafx@gmail.com>
2020-05-20 01:50:39 +0900

    CraftBukkitのパッチを当てる

--- scripts/install.sh
+++ scripts/install.sh
@@ -65,4 +65,17 @@ top="$(pwd -P)"
   if [ ! -d cb ]; then
     cp -r "$top"/modules/CraftBukkit cb
   fi
+
+  (
+    cd cb
+
+    src=src/main/java
+
+    rm -rf $src/net/minecraft/server
+    mkdir -p $src/minecraft/server
+    cp -rT "$working"/nms $src
+    find nms-patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | \
+      xargs -0 basename -z -s .patch | \
+      xargs -0 -I {} /bin/bash -c "patch $src/net/minecraft/server/{}.java < nms-patches/{}.patch"
+  )
 )
