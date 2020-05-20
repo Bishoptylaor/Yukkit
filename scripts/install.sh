ffa7ff9e6cc54a0a287f4708d10b5353fda11fef
YukiLeafX <yukileafx@gmail.com>
2020-05-20 20:57:37 +0900

    不要なエスケープを削除

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,25 +4,25 @@ set -ex
 
 scripts/init.sh
 
-working="$(pwd -P)"
-ver=$(jq -r '.minecraftVersion' modules/BuildData/info.json)
+working=$(pwd -P)
+ver=$(jq -r .minecraftVersion modules/BuildData/info.json)
 
 if [ ! -f nms-"$ver".jar ]; then
-  curl -o nms-"$ver".jar "$(jq -r '.serverUrl' modules/BuildData/info.json)"
-  [ "$(md5sum nms-"$ver".jar | cut -d ' ' -f 1)" = "$(jq -r '.minecraftHash' modules/BuildData/info.json)" ] || return
+  curl -o nms-"$ver".jar "$(jq -r .serverUrl modules/BuildData/info.json)"
+  [ "$(md5sum nms-"$ver".jar | cut -d ' ' -f 1)" = "$(jq -r .minecraftHash modules/BuildData/info.json)" ] || return
 fi
 
 if [ ! -f nms-"$ver"-class.jar ]; then
   java -jar modules/BuildData/bin/SpecialSource-2.jar map \
     -i nms-"$ver".jar \
-    -m modules/BuildData/mappings/"$(jq -r '.classMappings' modules/BuildData/info.json)" \
+    -m modules/BuildData/mappings/"$(jq -r .classMappings modules/BuildData/info.json)" \
     -o nms-"$ver"-class.jar
 fi
 
 if [ ! -f nms-"$ver"-member.jar ]; then
   java -jar modules/BuildData/bin/SpecialSource-2.jar map \
     -i nms-"$ver"-class.jar \
-    -m modules/BuildData/mappings/"$(jq -r '.memberMappings' modules/BuildData/info.json)" \
+    -m modules/BuildData/mappings/"$(jq -r .memberMappings modules/BuildData/info.json)" \
     -o nms-"$ver"-member.jar
 fi
 
@@ -30,8 +30,8 @@ if [ ! -f nms-"$ver"-mapped.jar ]; then
   java -jar modules/BuildData/bin/SpecialSource.jar \
     --kill-lvt \
     -i nms-"$ver"-member.jar \
-    --access-transformer modules/BuildData/mappings/"$(jq -r '.accessTransforms' modules/BuildData/info.json)" \
-    -m modules/BuildData/mappings/"$(jq -r '.packageMappings' modules/BuildData/info.json)" \
+    --access-transformer modules/BuildData/mappings/"$(jq -r .accessTransforms modules/BuildData/info.json)" \
+    -m modules/BuildData/mappings/"$(jq -r .packageMappings modules/BuildData/info.json)" \
     -o nms-"$ver"-mapped.jar
 fi
 
@@ -63,8 +63,8 @@ fi
 (
   cd modules/CraftBukkit
 
-  src='src/main/java'
-  nmspkg='net/minecraft/server'
+  src=src/main/java
+  nmspkg=net/minecraft/server
 
   # *craftbukkit
   git switch -C craftbukkit
@@ -78,13 +78,13 @@ fi
   rm -rf ${src:?}/$nmspkg
   mkdir -p $src/$nmspkg
 
-  find nms-patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | \
+  find nms-patches -mindepth 1 -maxdepth 1 -type f -iname '*'.patch -print0 | \
     xargs -0 -P 0 basename -z -s .patch | \
     xargs -0 -P 0 -I {} /bin/bash -c \
       "cp -v $working/nms/src/$nmspkg/{}.java $src/$nmspkg && patch $src/$nmspkg/{}.java < nms-patches/{}.patch"
 
   git add $src
-  git commit --message="NMS + CraftBukkit < nms-patches(CraftBukkit) | $(date)"
+  git commit --message "NMS + CraftBukkit < nms-patches(CraftBukkit) | $(date)"
 
   git switch --detach craftbukkit
 
@@ -93,7 +93,7 @@ fi
   git branch -D spigot || :
   git switch -C spigot
 
-  find "$working"/modules/Spigot/CraftBukkit-patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | \
+  find "$working"/modules/Spigot/CraftBukkit-patches -mindepth 1 -maxdepth 1 -type f -iname '*'.patch -print0 | \
     xargs -0 git am --3way || git am --quit
 
   git switch --detach craftbukkit
@@ -198,9 +198,9 @@ fi
     xargs -P 0 -I {} cp -nv "$working"/nms/src/$nmspkg/{}.java $src/$nmspkg
 
   git add $src
-  git commit --message="*spigot + mcdev imports | $(date)"
+  git commit --message "*spigot + mcdev imports | $(date)"
 
-  find "$working"/modules/Paper/Spigot-Server-Patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | \
+  find "$working"/modules/Paper/Spigot-Server-Patches -mindepth 1 -maxdepth 1 -type f -iname '*'.patch -print0 | \
     xargs -0 git am --3way || git am --quit
 
   git switch --detach craftbukkit
@@ -223,14 +223,14 @@ fi
   git switch bukkit
   git branch -D spigot-api || :
   git switch -C spigot-api
-  find "$working"/modules/Spigot/Bukkit-Patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | xargs -0 git am --3way || git am --quit
+  find "$working"/modules/Spigot/Bukkit-Patches -mindepth 1 -maxdepth 1 -type f -iname '*'.patch -print0 | xargs -0 git am --3way || git am --quit
   git switch --detach bukkit
 
   # *paper-api: *spigot-api < Spigot-API-Patches(Paper)
   git switch spigot-api
   git branch -D paper-api || :
   git switch -C paper-api
-  find "$working"/modules/Paper/Spigot-API-Patches -mindepth 1 -maxdepth 1 -type f -iname '*.patch' -print0 | xargs -0 git am --3way || git am --quit
+  find "$working"/modules/Paper/Spigot-API-Patches -mindepth 1 -maxdepth 1 -type f -iname '*'.patch -print0 | xargs -0 git am --3way || git am --quit
   git switch --detach bukkit
 
   # *yukkit-api
