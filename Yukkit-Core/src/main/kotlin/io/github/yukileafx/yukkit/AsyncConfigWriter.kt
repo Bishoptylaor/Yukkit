92a2f6e625de18d016a8f466dba04d4b67bf13b4
YukiLeafX <yukileafx@gmail.com>
2020-05-28 01:06:13 +0900

    Yukkit-Coreを移動してくる

---
+++ Yukkit-Core/src/main/kotlin/io/github/yukileafx/yukkit/AsyncConfigWriter.kt
@@ -0,0 +1,28 @@
+package io.github.yukileafx.yukkit
+
+import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.Executors
+import java.util.logging.Level
+
+object AsyncConfigWriter {
+
+    private val executor = Executors.newSingleThreadExecutor { r -> Thread(r, AsyncConfigWriter::class.simpleName) }
+
+    fun write(file: File, config: FileConfiguration) {
+        executor.submit {
+            val data = config.saveToString()
+            kotlin.runCatching {
+                file.canonicalFile.parentFile
+                    .also { it.mkdirs() }
+                    .takeIf { it.isDirectory }
+                    ?: throw IOException("Unable to create parent directories.")
+                file.writeText(data)
+            }.onFailure { e ->
+                Bukkit.getLogger().log(Level.SEVERE, "An error occurred while writing to $file.", e)
+            }
+        }
+    }
+}
