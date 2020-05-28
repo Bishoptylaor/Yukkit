0d1e7fbc27c6fcdd397ed48a4ba0d3681ca7022c
YukiLeafX <yukileafx@gmail.com>
2020-05-29 04:11:08 +0900

    Gradleに移行

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+group = "io.github.yukileafx"
+version = "1.12.2-R0.1-SNAPSHOT"
+
+subprojects {
+    group = parent?.group.toString()
+    version = parent?.version.toString()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "Yukkit"
+include("modules:Yukkit-Core")
+include("modules:Yukkit-API")
+include("modules:Yukkit-Server")
